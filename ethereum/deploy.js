6a1c641d4c658f115b1e2d779f25bd0c85c655a1
HughesSeanKyle <khughessean@yahoo.com>
2021-01-06 11:29:01 +0200

    V146 - Refactoring Deployment

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,9 +1,9 @@
 // V54 - Wallet Provider Setup
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const compiledFactory = require('./build/CampaignFactory.json');
 
-const keys = require('./config/keys');
+const keys = require('../config/keys');
 
 // HDWallet helps to generate public key, Pvt key and address of OUR account
 const provider = new HDWalletProvider(
@@ -22,10 +22,15 @@ const deploy = async () => {
     .deploy({ data: compiledFactory.bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
-    console.log(interface);
     console.log('Contract deployed to', result.options.address);
 };
 deploy();
 
+/*
+Deployed from
+0xdBB3C11A3ded56D19bc65F12FdA2811
 
+Address of newly created contract
+0x89C2555d4A0f0d5F85af7c34e267C3B46351893D
+ */
 
