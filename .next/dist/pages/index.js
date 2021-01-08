fbb6c332cc3487d5f79603600d305e88f36a7943
HughesSeanKyle <khughessean@yahoo.com>
2021-01-08 08:25:58 +0200

    V151 - Root Route

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'C:\\Users\\Kyle\\Desktop\\All docs needed2020.1.4\\Blockchain Course\\Stephen_Ethereum-Workspace\\Section_6-KickStarterProject\\pages\\index.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 4
+        }
+    }, 'This is the campaign list page!!');
+};
+
+// Next.js detects this page as the Root Route based on the naming inside the pages direcory.
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTyxBQUVQOzs7Ozs7OztrQkFBZSxZQUFNLEFBQ2pCOzJCQUFPLGNBQUE7O3NCQUFBO3dCQUFBO0FBQUE7QUFBQSxLQUFBLEVBQVAsQUFBTyxBQUNWO0FBRkQ7O0FBSUEiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvS3lsZS9EZXNrdG9wL0FsbCBkb2NzIG5lZWRlZDIwMjAuMS40L0Jsb2NrY2hhaW4gQ291cnNlL1N0ZXBoZW5fRXRoZXJldW0tV29ya3NwYWNlL1NlY3Rpb25fNi1LaWNrU3RhcnRlclByb2plY3QifQ==
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+    return <h1>This is the campaign list page!!</h1>;
+};
+
+// Next.js detects this page as the Root Route based on the naming inside the pages direcory. 
