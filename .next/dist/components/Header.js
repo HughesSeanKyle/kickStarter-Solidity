c71c0b4ea589efd9b856cd3d7eccafb50dbf81e9
HughesSeanKyle <khughessean@yahoo.com>
2021-01-18 08:38:11 +0200

    V165 - Assembling a Header

---
+++ .next/dist/components/Header.js
@@ -0,0 +1,44 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _semanticUiReact = require('semantic-ui-react');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'C:\\Users\\Kyle\\Desktop\\All docs needed2020.1.4\\Blockchain Course\\Stephen_Ethereum-Workspace\\Section_6-KickStarterProject\\components\\Header.js';
+
+exports.default = function () {
+    return _react2.default.createElement(_semanticUiReact.Menu, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 6
+        }
+    }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 7
+        }
+    }, 'CrowdCoin'), _react2.default.createElement(_semanticUiReact.Menu.Menu, { position: 'right', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 11
+        }
+    }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 12
+        }
+    }, 'Campaigns'), _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 16
+        }
+    }, '+')));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXEhlYWRlci5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIk1lbnUiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTLEFBRVQ7Ozs7OztrQkFBZSxZQUFNLEFBQ2pCOzJCQUNJLEFBQUM7O3NCQUFEO3dCQUFBLEFBQ0k7QUFESjtBQUFBLEtBQUEsa0JBQ0ssY0FBRCxzQkFBQSxBQUFNOztzQkFBTjt3QkFBQTtBQUFBO0FBQUEsT0FESixBQUNJLEFBSUEsOEJBQUMsY0FBRCxzQkFBQSxBQUFNLFFBQUssVUFBWCxBQUFvQjtzQkFBcEI7d0JBQUEsQUFDSTtBQURKO3VCQUNLLGNBQUQsc0JBQUEsQUFBTTs7c0JBQU47d0JBQUE7QUFBQTtBQUFBLE9BREosQUFDSSxBQUlBLDhCQUFDLGNBQUQsc0JBQUEsQUFBTTs7c0JBQU47d0JBQUE7QUFBQTtBQUFBLE9BWFosQUFDSSxBQUtJLEFBS0ksQUFNZjtBQWxCRCIsImZpbGUiOiJIZWFkZXIuanMiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvS3lsZS9EZXNrdG9wL0FsbCBkb2NzIG5lZWRlZDIwMjAuMS40L0Jsb2NrY2hhaW4gQ291cnNlL1N0ZXBoZW5fRXRoZXJldW0tV29ya3NwYWNlL1NlY3Rpb25fNi1LaWNrU3RhcnRlclByb2plY3QifQ==
--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 export default (props) => {
     return (
         <div>
-            <h1>I am a Header</h1>
-                {props.children}
-            <h1>I am a Footer</h1>
+            <Header />
+                {props.children} 
         </div>
     );
 };
