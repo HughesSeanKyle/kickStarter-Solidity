7ecb7f50a5cf11d1a841a4dbe7d9dbd36d5f2c37
HughesSeanKyle <khughessean@yahoo.com>
2021-01-19 09:33:24 +0200

    V168 - Nested Routing

---
+++ pages/campaigns/new.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class CampaignNew extends Component {
+    render () {
+        return <div>New Campaign!</div>; 
+    }
+}
+
+export default CampaignNew;
