d080d6f2abe48cf5c59f838b07d388baef8eeca1
HughesSeanKyle <khughessean@yahoo.com>
2021-01-22 10:52:45 +0200

    V178 - Automatic Navigation

---
+++ .next/dist/routes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var routes = require('next-routes')(); // 1.1
+
+
+module.exports = routes;
+
+/*
+/1.1
+Second set of () resturns a function. 
+ */
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZixBLDBCQUF5Qzs7O0FBTXpDLE9BQU8sQUFBUCxVQUFpQixBQUFqQjs7QUFFQSIsImZpbGUiOiJyb3V0ZXMuanMiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvS3lsZS9EZXNrdG9wL0FsbCBkb2NzIG5lZWRlZDIwMjAuMS40L0Jsb2NrY2hhaW4gQ291cnNlL1N0ZXBoZW5fRXRoZXJldW0tV29ya3NwYWNlL1NlY3Rpb25fNi1LaWNrU3RhcnRlclByb2plY3QifQ==
