9365328f3735da7b150f56db3581c658be64ce82
HughesSeanKyle <khughessean@yahoo.com>
2021-01-23 09:42:26 +0200

    V182 - Route Mapping

--- .next/dist/routes.js
+++ .next/dist/routes.js
@@ -2,11 +2,16 @@
 
 var routes = require('next-routes')(); // 1.1
 
+routes.add('/campaigns/new', '/campaigns/new').add('/campaigns/:address', 'campaigns/show'); // 1.2
 
 module.exports = routes;
 
 /*
-/1.1
+//1.1
 Second set of () resturns a function. 
+
+// 1.2
+Colon in url indicates that this part of the url will ba a wildcard or variable in nature. 
+Second argument indicates what component to route to. 
  */
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZixBLDBCQUF5Qzs7O0FBTXpDLE9BQU8sQUFBUCxVQUFpQixBQUFqQjs7QUFFQSIsImZpbGUiOiJyb3V0ZXMuanMiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvS3lsZS9EZXNrdG9wL0FsbCBkb2NzIG5lZWRlZDIwMjAuMS40L0Jsb2NrY2hhaW4gQ291cnNlL1N0ZXBoZW5fRXRoZXJldW0tV29ya3NwYWNlL1NlY3Rpb25fNi1LaWNrU3RhcnRlclByb2plY3QifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZixBLDBCQUF5Qzs7QUFFekMsT0FDSyxBQURMLElBQ1MsQUFEVCxrQkFDMkIsQUFEM0Isa0JBRUssQUFGTCxJQUVTLEFBRlQsdUJBRWdDLEFBRmhDLEEsbUJBRW1EOztBQUVuRCxPQUFPLEFBQVAsVUFBaUIsQUFBakI7O0FBRUEiLCJmaWxlIjoicm91dGVzLmpzIiwic291cmNlUm9vdCI6IkM6L1VzZXJzL0t5bGUvRGVza3RvcC9BbGwgZG9jcyBuZWVkZWQyMDIwLjEuNC9CbG9ja2NoYWluIENvdXJzZS9TdGVwaGVuX0V0aGVyZXVtLVdvcmtzcGFjZS9TZWN0aW9uXzYtS2lja1N0YXJ0ZXJQcm9qZWN0In0=
--- routes.js
+++ routes.js
@@ -1,12 +1,16 @@
 const routes = require('next-routes')(); // 1.1
 
-
-
-
+routes
+    .add('/campaigns/new', '/campaigns/new')
+    .add('/campaigns/:address', 'campaigns/show'); // 1.2
 
 module.exports = routes;
 
 /*
-/1.1
+//1.1
 Second set of () resturns a function. 
+
+// 1.2
+Colon in url indicates that this part of the url will ba a wildcard or variable in nature. 
+Second argument indicates what component to route to. 
  */
